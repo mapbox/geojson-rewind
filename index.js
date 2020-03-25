a2b44259f795082e0ad47ad745ce9a5897a63358
Vladimir Agafonkin <agafonkin@gmail.com>
2020-03-25 12:32:07 +0200

    simplify rewinding code, get rid of geojson-area dep

--- index.js
+++ index.js
@@ -1,52 +1,39 @@
-var geojsonArea = require('@mapbox/geojson-area');
 
 module.exports = rewind;
 
 function rewind(gj, outer) {
-    switch ((gj && gj.type) || null) {
-        case 'FeatureCollection':
-            gj.features = gj.features.map(curryOuter(rewind, outer));
-            return gj;
-        case 'GeometryCollection':
-            gj.geometries = gj.geometries.map(curryOuter(rewind, outer));
-            return gj;
-        case 'Feature':
-            gj.geometry = rewind(gj.geometry, outer);
-            return gj;
-        case 'Polygon':
-        case 'MultiPolygon':
-            return correct(gj, outer);
-        default:
-            return gj;
+    var type = gj && gj.type, i;
+
+    if (type === 'FeatureCollection') {
+        for (i = 0; i < gj.features.length; i++) rewind(gj.features[i], outer);
+
+    } else if (type === 'GeometryCollection') {
+        for (i = 0; i < gj.geometries.length; i++) rewind(gj.geometries[i], outer);
+
+    } else if (type === 'Feature') {
+        rewind(gj.geometry, outer);
+
+    } else if (type === 'Polygon') {
+        rewindRings(gj.coordinates, outer);
+
+    } else if (type === 'MultiPolygon') {
+        for (i = 0; i < gj.coordinates.length; i++) rewindRings(gj.coordinates[i], outer);
+    }
+
+    return gj;
+}
+
+function rewindRings(rings, outer) {
+    rewindRing(rings[0], outer);
+    for (var i = 1; i < rings.length; i++) {
+        rewindRing(rings[i], !outer);
     }
 }
 
-function curryOuter(a, b) {
-    return function(_) { return a(_, b); };
-}
-
-function correct(_, outer) {
-    if (_.type === 'Polygon') {
-        _.coordinates = correctRings(_.coordinates, outer);
-    } else if (_.type === 'MultiPolygon') {
-        _.coordinates = _.coordinates.map(curryOuter(correctRings, outer));
+function rewindRing(ring, dir) {
+    var area = 0;
+    for (var i = 0, len = ring.length, j = len - 1; i < len; j = i++) {
+        area += (ring[i][0] - ring[j][0]) * (ring[j][1] + ring[i][1]);
     }
-    return _;
-}
-
-function correctRings(_, outer) {
-    outer = !!outer;
-    _[0] = wind(_[0], outer);
-    for (var i = 1; i < _.length; i++) {
-        _[i] = wind(_[i], !outer);
-    }
-    return _;
-}
-
-function wind(_, dir) {
-    return cw(_) === dir ? _ : _.reverse();
-}
-
-function cw(_) {
-    return geojsonArea.ring(_) >= 0;
+    if (area >= 0 !== !!dir) ring.reverse();
 }
